13a7e76cfc2031c2b6e08593334683c18261caaf
Adeja Lendor <alendor6@gmail.com>
2019-08-31 16:46:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
 yield(string)
 end
-reverse_each_word().each do |reverse_each_word|
+reverse_each_word("Hello there, and how are you?").each do |reverse_each_word|
   puts #{reverse_each_word.to_a}.reverse
